e41937657d27092bdedca52a8026fc60d931972f
Judit <mlle.judith.nagy@gmail.com>
2020-07-27 20:49:02 +0200

    navigation hide-bug fixed

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,16 +2,22 @@ class MobileMenu {
   constructor() {
     this.menuIcon = document.querySelector(".menu-icon")
     this.mainMenu = document.querySelector(".main-menu")
+    this.menuItem = document.querySelectorAll(".main-menu__item")
     this.events()
   }
 
   events() {
     this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+    this.menuItem.forEach(el => el.addEventListener("click", () => this.hideTheMenu()))
   }
 
   toggleTheMenu() {
     this.mainMenu.classList.toggle("main-menu--is-open")
     this.menuIcon.classList.toggle("menu-icon--close-x")
+  }
+  hideTheMenu() {
+    this.mainMenu.classList.remove("main-menu--is-open")
+    this.menuIcon.classList.remove("menu-icon--close-x")
   }
 }
 
