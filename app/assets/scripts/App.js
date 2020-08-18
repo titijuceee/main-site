8c674c981a08061e3d0ff0f3694f410e86b18752
Judit Titi <hello@titijudit.com>
2020-08-18 15:31:03 +0200

    finished reveal item function

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,13 @@ import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import ContactMenu from './modules/ContactMenu'
 import Modal from './modules/Modal'
+import RevealItems from './modules/RevealItems'
 
 new MobileMenu();
 new ContactMenu();
 new Modal();
+new RevealItems(document.querySelectorAll(".section__img"), 55);
+new RevealItems(document.querySelectorAll(".cards__item"), 75);
 
 if (module.hot) {
     module.hot.accept()
